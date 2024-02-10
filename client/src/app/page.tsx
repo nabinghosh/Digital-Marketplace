517a1b9819a97bc9a8012fcba0e2a1fce1109857
nabinghosh <www.champaghosha.12345@gmail.com>
2024-02-10 22:19:26 +0530

    basic styling like, theme and layouts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import React from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hi I am Nabin</p>
+    <main className="flex min-h-screen flex-col ">
+      <Card>
+        <CardHeader>
+          <h1>Welcome to our Landing Page</h1>
+        </CardHeader>
+        <CardBody>
+          <p>
+            This is a sample landing page. 
+          </p>
+          <Image
+            src="/path/to/your/image.jpg"
+            alt="Landing Page Image"
+            width={500}
+            height={300}
+          />
+        </CardBody>
+      </Card>
     </main>
   );
 }
